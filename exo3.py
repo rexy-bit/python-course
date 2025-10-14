5bb3ae76e3a921566027661a7e82fd09361eca35
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-10-14 22:59:40 +0100

    Add files via upload

---
+++ exo3.py
@@ -0,0 +1,7 @@
+
+
+L = float(input("Veillez entrez la longeur du rectangle : "))
+l = float(input("Veillez entrez la largeur du rectangle : "))
+
+print(f"\nLa surface du rectangle est : {format(L*l, ".2f")} m^2")
+print(f"\nLe perimettre du rectangle est de : {format((L + l)*2, ".2f")} m")
