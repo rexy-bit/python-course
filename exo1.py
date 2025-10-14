8d78ccbe712833cba1637865be4809157fe9f393
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-10-14 22:59:23 +0100

    Add files via upload

---
+++ exo1.py
@@ -0,0 +1,10 @@
+
+name = str(input("Entrez votre nom : "))
+age = int(input("Donnez votre age : "))
+
+
+def Bonjour(name, age):
+    print(f"\nBonjour {name} vous avez {age} ans Benvenutto to Universita")
+
+
+Bonjour(name, age)
