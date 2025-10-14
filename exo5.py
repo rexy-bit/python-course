16d4cc719796e600a0b8654c2a4ff4632996a479
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-10-14 22:59:56 +0100

    Add files via upload

---
+++ exo5.py
@@ -0,0 +1,8 @@
+
+a = float(input("\nEntrez le premier nombre : "))
+b = float(input("Entrez le deuxieme nombre : "))
+
+print(f"La somme : {format(a + b, '.2f')}")
+print(f"Le produit : {format(a*b, '.2f')}")
+print(f"La difference : {format(a - b, '.2f')}")
+print(f"La division : {format(a/b, '.2f')}")
